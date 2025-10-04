d1da3615123e9fe85fbf81c5b6bc9e22fd2d1981
kianosaurus25 <kianosaurus25@gmail.com>
2025-10-04 13:46:23 +0800

    Update CS.py

--- CS.py
+++ CS.py
@@ -1,32 +1,42 @@
-print("MENU")
-print("WELCOME TO U.B.E.T")
-print("1) View plant group library ")
-print("2) Identify plant needs ")
-print("3) Quit")
+mport time
 
-menu = int(input("What would you like to do? <3: "))
-if menu == 1:
-    print("ANGIOSPERMS")
-    print("BRYOPHYTA")
-    print("GYMNOSPERMS")
-    print("PTERIDOPHYTES")
-    print("CYCADOPHYTA")
-    print("HORSETAILS")
-    print("LIVERWORTS")
-    print("TREES")
-elif menu == 2:
-    print("What category best describes your plant?")
-    print("1]ANGIOSPERMS")
-    print("2]BRYOPHYTA")
-    print("3]GYMNOSPERMS")
-    print("4]PTERIDOPHYTES")
-    print("5]CYCADOPHYTA")
-    print("6]HORSETAILS")
-    print("7]LIVERWORTS")
-    print("8]TREES")
+program_on = True
+while program_on == True:
+    print("MENU")
+    print("WELCOME TO U.B.E.T")
+    print("1) View plant group library ")
+    print("2) Identify plant needs ")
+    print("3) Quit")
 
-elif menu == 3:
+    menu = int(input("What would you like to do <3?: "))
+    if menu == 1:
+        print("1]ANGIOSPERMS")
+        print("2]BRYOPHYTA")
+        print("3]GYMNOSPERMS")
+        print("4]PTERIDOPHYTES")
+        print("5]CYCADOPHYTA")
+        print("6]HORSETAILS")
+        print("7]LIVERWORTS")
+        print("8]TREES")
+        plant_category = int(input("See description of which plant group?"))
+        if plant_category == 1:
+            print("goo goo gaga")
 
-else:
-    print("Bye")
-print("anjelo is an unc")
+    elif menu == 2:
+        print()
+        print("What category best describes your plant?")
+        print("1]ANGIOSPERMS")
+        print("2]BRYOPHYTA")
+        print("3]GYMNOSPERMS")
+        print("4]PTERIDOPHYTES")
+        print("5]CYCADOPHYTA")
+        print("6]HORSETAILS")
+        print("7]LIVERWORTS")
+        print("8]TREES")
+        time.sleep(3)
+
+    elif menu == 3:
+        program_on = False
+    else:
+        print("Invalid input! Please try again")
+        print()
