ac70ace75c692c6a1a14bd7320576f982717cf5c
kianosaurus25 <kianosaurus25@gmail.com>
2025-10-04 13:14:22 +0800

    Add files via upload

---
+++ CS.py
@@ -0,0 +1,32 @@
+print("MENU")
+print("WELCOME TO U.B.E.T")
+print("1) View plant group library ")
+print("2) Identify plant needs ")
+print("3) Quit")
+
+menu = int(input("What would you like to do? <3: "))
+if menu == 1:
+    print("ANGIOSPERMS")
+    print("BRYOPHYTA")
+    print("GYMNOSPERMS")
+    print("PTERIDOPHYTES")
+    print("CYCADOPHYTA")
+    print("HORSETAILS")
+    print("LIVERWORTS")
+    print("TREES")
+elif menu == 2:
+    print("What category best describes your plant?")
+    print("1]ANGIOSPERMS")
+    print("2]BRYOPHYTA")
+    print("3]GYMNOSPERMS")
+    print("4]PTERIDOPHYTES")
+    print("5]CYCADOPHYTA")
+    print("6]HORSETAILS")
+    print("7]LIVERWORTS")
+    print("8]TREES")
+
+elif menu == 3:
+
+else:
+    print("Bye")
+print("anjelo is an unc")
